2556c320245aebc044441289b1e87c37719a3e5e
seyoungjoy <diddpwl80@gmail.com>
2022-11-01 23:37:49 +0900

    :art: Modify file name

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ import Profile from './pages/Profile';
 import NoMatch from './components/common/NoMatch';
 import { useMobileCheck, isMobile } from './hooks/useMobile';
 import OnBoarding from './pages/OnBoarding';
-import LoginStep1 from './pages/LoginStep1';
-import LoginStep2 from './pages/LoginStep2';
+import Landing from './pages/Landing';
+import Login from './pages/Login';
 import Join from './pages/Join';
 
 const App = () => {
@@ -19,8 +19,8 @@ const App = () => {
       path: '/',
       element: <Layout />,
       children: [
-        { index: true, element: <LoginStep1 /> },
-        { path: 'login', element: <LoginStep2 /> },
+        { index: true, element: <Landing /> },
+        { path: 'login', element: <Login /> },
         { path: 'home', element: <Home /> },
         { path: 'join', element: <Join /> },
         { path: 'favorite', element: <Favorite /> },
