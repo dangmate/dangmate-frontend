151032a1953cdb14e63b8c774d6ecb168b34d918
seyoungjoy <diddpwl80@gmail.com>
2022-11-12 01:25:59 +0900

    :art: Add comment component

--- src/pages/HomeView.tsx
+++ src/pages/HomeView.tsx
@@ -4,6 +4,7 @@ import { getVwValue } from '../styles/styleUtil';
 import { useNavigate } from 'react-router-dom';
 import FeedView from '../components/section/home/FeedView';
 import CommentState from '../components/section/comment/CommentState';
+import Comment from '../components/section/comment/Comment';
 import UserName from '../components/section/home/UserName';
 import PostTime from '../components/section/home/PostTime';
 import ButtonMore from '../components/asset/ButtonMore';
@@ -48,62 +49,10 @@ const HomeView = () => {
       <S.Container>
         <FeedView />
         <CommentState />
-        <CommentArea />
+        <div>
+          <Comment />
+        </div>
       </S.Container>
-    </>
-  );
-};
-
-const S2 = {
-  Container: styled.div``,
-  Head: styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: ${getVwValue('15')};
-  `,
-  Column: styled.div`
-    display: flex;
-  `,
-  TimeWrap: styled.div`
-    margin-left: ${getVwValue('10')};
-  `,
-  Content: styled.div`
-    padding: ${getVwValue('0 0 0 12')};
-  `,
-  Foot: styled.div``
-};
-
-const CommentArea = () => {
-  return (
-    <>
-      <S2.Container>
-        <S2.Head>
-          <S2.Column>
-            <UserName
-              src={'/images/profile.png'}
-              alt={'profile'}
-              name={'소심쟁이 제이'}
-            ></UserName>
-            <S2.TimeWrap>
-              <PostTime />
-            </S2.TimeWrap>
-          </S2.Column>
-
-          <S2.Column>
-            <ButtonMore />
-          </S2.Column>
-        </S2.Head>
-
-        <S2.Content>
-          <span>
-            OO동에 이사온지 얼마 되지 않아 아는 친구가 한 명도 없어요ㅜㅜ나이는
-            25살 여자입니다! 같이 밥먹고 운동 다닐 동네 친구 구해요! 또래에 산책
-            같이 마음 맞는 분 찾으면 좋겠어요!
-          </span>
-          <div>footer</div>
-        </S2.Content>
-      </S2.Container>
     </>
   );
 };
